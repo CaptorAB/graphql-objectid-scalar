547d26d8ee39f6641b4f02569f530b2df42a55be
Andreas Lindh <andreas.lindh@loopstone.com>
2019-12-16 11:42:00 +0100

    publish

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { GraphQLScalarType } from "graphql";
+import { Kind } from "graphql/language";
+import { GraphQLError } from "graphql/error";
+import { ObjectId } from "mongodb";
+
+export function isValidMongoDBObjectID(id) {
+    /// https://stackoverflow.com/questions/11985228/mongodb-node-check-if-objectid-is-valid
+    id = id + "";
+    var len = id.length,
+        valid = false;
+    if (len === 12 || len === 24) {
+        valid = /^[0-9a-fA-F]+$/.test(id);
+    }
+    return valid;
+}
+
+export const GraphQLObjectId = new GraphQLScalarType({
+    name: "GraphQLObjectId",
+    description: "GraphQLObjectId is a mongodb ObjectId. String of 12 or 24 hex chars",
+
+    // from database towards client
+    serialize(value) {
+        let result = value.toString();
+        if (!isValidMongoDBObjectID(result)) {
+            throw new GraphQLError("serialize: value: " + value + " is not valid object id");
+        }
+
+        return result;
+    },
+
+    // json from client towards database
+    parseValue(value) {
+        if (!isValidMongoDBObjectID(value)) {
+            throw new GraphQLError("serialize: not a valid ObjectId, require a string with 12 or 24 hex chars, found: " + value);
+        }
+
+        return new ObjectId(value);
+    },
+
+    // AST from client towards database
+    parseLiteral(ast) {
+        if (ast.kind !== Kind.STRING) {
+            throw new GraphQLError("parseLiteral: not a valid ObjectId, require a string with 12 or 24 hex chars, found: " + ast.kind, [
+                ast
+            ]);
+        }
+
+        const value = ast.value.toString();
+        return new ObjectId(value);
+    }
+});
+
+export default GraphQLObjectId;
---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+import { GraphQLError } from "graphql/error";
+import { Kind } from "graphql/language";
+import { ObjectId } from "mongodb";
+import { GraphQLObjectId } from "../src";
+
+const { serialize, parseValue, parseLiteral } = GraphQLObjectId;
+
+describe("graphql-objectid-scalar", () => {
+    test("Should serialize", () => {
+        const id = new ObjectId();
+
+        const value = serialize(id);
+
+        expect(value).toEqual(id.toString());
+    });
+
+    test("parseValue should reject garbage", async () => {
+        await expect(() => parseValue("garbage").toThrow(new GraphQLError()));
+    });
+
+    test("parseLiteral should reject garbage", async () => {
+        await expect(() => parseLiteral({ value: "garbage", kind: Kind.STRING }, {}).toThrow(new GraphQLError()));
+    });
+
+    test("parseValue should return an ObjectId object", () => {
+        const id = new ObjectId();
+        const result = parseValue(id.toString());
+
+        expect(result.toString()).toEqual(id.toString());
+        expect(result.constructor).toEqual(ObjectId);
+    });
+
+    test("parseLiteral should return an ObjectId object", () => {
+        const id = new ObjectId();
+        const result = parseLiteral({ value: id.toString(), kind: Kind.STRING }, {});
+
+        expect(result.toString()).toEqual(id.toString());
+        expect(result.constructor).toEqual(ObjectId);
+    });
+});
